b22b17f54ed0e51d0b908abe97020a72db7fbaa7
Halil Azyikmis <hazyi@halo-pc-hp.hacettepe.edu.tr>
2020-07-09 14:18:27 +0200

    try on minAvgTwoSlice problem

--- src.js
+++ src.js
@@ -739,3 +739,12 @@ function minAvgTwoSlice(A) {
   }
   return minIdx;
 }
+
+
+[1, 2, 2, 5, 1, 5, 8, 4, 2, -2]
+
+total 10 (0 <= idx <= 9)
+start   end     total       avg         minIdx
+0       1       1+2=3       3/2=1.5     0
+0       2       1+2+2=5     5/3=1.7     0
+0       3       1+2+2+5=10  10/4=2.5    0
