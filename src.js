14469c9be7c6a461de8be6046da5c6a26ae1085f
Halil Azyikmis <hazyi@halo-pc-hp.hacettepe.edu.tr>
2020-07-09 21:56:49 +0200

    Distinct

--- src.js
+++ src.js
@@ -49,12 +49,10 @@ For another example, given
     A = [0, 0, 0]
     K = 1
 the function should return [0, 0, 0]
-
 Given
     A = [1, 2, 3, 4]
     K = 4
 the function should return [1, 2, 3, 4]
-
 Assume that:
 N and K are integers within the range [0..100];
 each element of array A is an integer within the range [−1,000..1,000].
@@ -783,3 +781,27 @@ function passingCars(A) {
   return totalCouples > 1000000000 ? -1 : totalCouples;
 }
 
+//# Distinct
+//Compute number of distinct values in an array.
+/*
+Write a function
+function solution(A);
+that, given an array A consisting of N integers, returns the number of distinct values in array A.
+For example, given array A consisting of six elements such that:
+ A[0] = 2    A[1] = 1    A[2] = 1
+ A[3] = 2    A[4] = 3    A[5] = 1
+the function should return 3, because there are 3 distinct values appearing in array A, namely 1, 2 and 3.
+Write an efficient algorithm for the following assumptions:
+N is an integer within the range [0..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+*/
+function distinct(A) {
+  const countObj = A.reduce((acc, cur) => {
+    acc[cur] ? acc[cur]++ : (acc[cur] = 1);
+    return acc;
+  }, {});
+  //countObj is {0: 1, 2: 1, 3: 3, 4: 1, 5: 1, 7: 2, 9: 1}
+  let countObjKeysArr = Object.keys(countObj);
+  //countObjKeysArr is ["0", "2", "3", "4", "5", "7", "9"]
+  return countObjKeysArr.length;
+}
