6276e9b567b1c557ac7d1f4daf7e088d5250c876
Halil Azyikmis <hazyi@halo-pc-hp.hacettepe.edu.tr>
2020-07-09 22:49:29 +0200

    MaxProductOfThree

--- src.js
+++ src.js
@@ -805,3 +805,45 @@ function distinct(A) {
   //countObjKeysArr is ["0", "2", "3", "4", "5", "7", "9"]
   return countObjKeysArr.length;
 }
+
+//# MaxProductOfThree
+//Maximize A[P] * A[Q] * A[R] for any triplet (P, Q, R).
+/*
+A non-empty array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
+For example, array A such that:
+  A[0] = -3
+  A[1] = 1
+  A[2] = 2
+  A[3] = -2
+  A[4] = 5
+  A[5] = 6
+contains the following example triplets:
+(0, 1, 2), product is −3 * 1 * 2 = −6
+(1, 2, 4), product is 1 * 2 * 5 = 10
+(2, 4, 5), product is 2 * 5 * 6 = 60
+Your goal is to find the maximal product of any triplet.
+Write a function:
+function solution(A);
+that, given a non-empty array A, returns the value of the maximal product of any triplet.
+For example, given array A such that:
+  A[0] = -3
+  A[1] = 1
+  A[2] = 2
+  A[3] = -2
+  A[4] = 5
+  A[5] = 6
+the function should return 60, as the product of triplet (2, 4, 5) is maximal.
+Write an efficient algorithm for the following assumptions:
+N is an integer within the range [3..100,000];
+each element of array A is an integer within the range [−1,000..1,000].
+*/
+
+function maxProductOfThree(A) {
+  let sortedA = A.sort((a, b) => b - a);
+  //return sortedA[0] * sortedA[1] * sortedA[2];
+
+  let last2Prod = sortedA[A.length-2] * sortedA[A.length-1];
+  let first2ndAnd3rdProd = sortedA[1] * sortedA[2];
+  //sortedA[0] > 0 checked because of the chance that all items are negative
+  return last2Prod > first2ndAnd3rdProd &&  sortedA[0] > 0 ? sortedA[0] * last2Prod : sortedA[0] * first2ndAnd3rdProd;
+}
