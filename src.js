95b45fe74d7a7955537ef9bad4792b004de8fc5c
Halil Azyikmis <hazyi@halo-pc-hp.hacettepe.edu.tr>
2020-07-11 04:23:51 +0200

    NumberOfDiscIntersections

--- src.js
+++ src.js
@@ -720,9 +720,9 @@ function minAvgTwoSlice(A) {
   let minIdx = 0;
   let minAvg = (A[0] + A[1]) / 2;
 
-  for (let i = 0; i <= A.length-1; i++) {
+  for (let i = 0; i <= A.length - 1; i++) {
     prevSum = A[i];
-    for (let j = i+1; j <= A.length; j++) {
+    for (let j = i + 1; j <= A.length; j++) {
       prevSum = prevSum + A[j];
       avg = prevSum / (j - i + 1);
       if (avg < minAvg) {
@@ -776,8 +776,8 @@ each element of array A is an integer that can have one of the following values:
 function passingCars(A) {
   let totalCouples = 0;
   let totalOnes = 0;
-  for (let i=A.length-1; i>=0; i--) 
-    A[i] === 0 ? totalCouples = totalCouples + totalOnes : totalOnes++;
+  for (let i = A.length - 1; i >= 0; i--)
+    A[i] === 0 ? (totalCouples = totalCouples + totalOnes) : totalOnes++;
   return totalCouples > 1000000000 ? -1 : totalCouples;
 }
 
@@ -842,8 +842,69 @@ function maxProductOfThree(A) {
   let sortedA = A.sort((a, b) => b - a);
   //return sortedA[0] * sortedA[1] * sortedA[2];
 
-  let last2Prod = sortedA[A.length-2] * sortedA[A.length-1];
+  let last2Prod = sortedA[A.length - 2] * sortedA[A.length - 1];
   let first2ndAnd3rdProd = sortedA[1] * sortedA[2];
   //sortedA[0] > 0 checked because of the chance that all items are negative
-  return last2Prod > first2ndAnd3rdProd &&  sortedA[0] > 0 ? sortedA[0] * last2Prod : sortedA[0] * first2ndAnd3rdProd;
+  return last2Prod > first2ndAnd3rdProd && sortedA[0] > 0
+    ? sortedA[0] * last2Prod
+    : sortedA[0] * first2ndAnd3rdProd;
+}
+
+//# NumberOfDiscIntersections
+//Compute the number of intersections in a sequence of discs.
+/*
+We draw N discs on a plane. The discs are numbered from 0 to N − 1. An array A of N non-negative integers, specifying the radiuses of the discs, is given. The J-th disc is drawn with its center at (J, 0) and radius A[J].
+We say that the J-th disc and K-th disc intersect if J ≠ K and the J-th and K-th discs have at least one common point (assuming that the discs contain their borders).
+The figure below shows discs drawn for N = 6 and A as follows:
+  A[0] = 1
+  A[1] = 5
+  A[2] = 2
+  A[3] = 1
+  A[4] = 4
+  A[5] = 0
+There are eleven (unordered) pairs of discs that intersect, namely:
+discs 1 and 4 intersect, and both intersect with all the other discs;
+disc 2 also intersects with discs 0 and 3.
+Write a function:
+function solution(A);
+that, given an array A describing N discs as explained above, returns the number of (unordered) pairs of intersecting discs. The function should return −1 if the number of intersecting pairs exceeds 10,000,000.
+Given array A shown above, the function should return 11, as explained above.
+Write an efficient algorithm for the following assumptions:
+N is an integer within the range [0..100,000];
+each element of array A is an integer within the range [0..2,147,483,647].
+*/
+
+function numberOfDiscIntersections(A) {
+  //generate new arrays for start & end indexes
+  let startA = [];
+  let endA = [];
+  for (let i = 0; i < A.length; i++) {
+    startA.push(i - A[i]);
+    endA.push(i + A[i]);
+  }
+
+  //sort startA & endA (both ASC)
+  startA.sort((a, b) => a - b);
+  endA.sort((a, b) => a - b);
+
+  //calculate intersections
+  let lenA = A.length;
+  let numOfOpenDiscs = 0;
+  let numOfIntersections = 0;
+  let startIdx = 0;
+  let endIdx = 0;
+  while (startIdx < lenA && endIdx < lenA) {
+    while (startA[startIdx] <= endA[endIdx]) {
+      startIdx++;
+      numOfIntersections += numOfOpenDiscs;
+      numOfOpenDiscs++;
+      //console.log("Inc", [startIdx, endIdx, numOfOpenDiscs, numOfIntersections ]);
+    }
+    while (startA[startIdx] > endA[endIdx]) {
+      endIdx++;
+      numOfOpenDiscs--;
+      //console.log("Dec", [startIdx, endIdx, numOfOpenDiscs, numOfIntersections ]);
+    }
+  }
+  return numOfIntersections > 10000000 ? -1 : numOfIntersections;
 }
