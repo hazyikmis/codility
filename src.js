125e9a580c0f35f7b861d7dd5fc2e7462943067a
Halil Azyikmis <hazyi@halo-pc-hp.hacettepe.edu.tr>
2020-07-11 16:18:58 +0200

    Triangle

--- src.js
+++ src.js
@@ -908,3 +908,43 @@ function numberOfDiscIntersections(A) {
   }
   return numOfIntersections > 10000000 ? -1 : numOfIntersections;
 }
+
+//# Triangle
+//Determine whether a triangle can be built from a given set of edges.
+/*
+An array A consisting of N integers is given. A triplet (P, Q, R) is triangular if 0 ≤ P < Q < R < N and:
+A[P] + A[Q] > A[R],
+A[Q] + A[R] > A[P],
+A[R] + A[P] > A[Q].
+For example, consider array A such that:
+  A[0] = 10    A[1] = 2    A[2] = 5
+  A[3] = 1     A[4] = 8    A[5] = 20
+Triplet (0, 2, 4) is triangular.
+Write a function:
+function solution(A);
+that, given an array A consisting of N integers, returns 1 if there exists a triangular triplet for this array and returns 0 otherwise.
+For example, given array A such that:
+  A[0] = 10    A[1] = 2    A[2] = 5
+  A[3] = 1     A[4] = 8    A[5] = 20
+the function should return 1, as explained above. Given array A such that:
+  A[0] = 10    A[1] = 50    A[2] = 5
+  A[3] = 1
+the function should return 0.
+Write an efficient algorithm for the following assumptions:
+N is an integer within the range [0..100,000];
+each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
+*/
+
+function triangle(A) {
+  //remove 0s and negative values
+  //order all numbers and check if A[n] < A[n+1] + A[n+2]
+  let posA = A.filter((a) => a > 0);
+  let lenPosA = posA.length;
+  if (lenPosA < 3) return 0;
+  let ordPosA = posA.sort((a, b) => b - a); //desc sort
+
+  for (let i = 0; i < lenPosA - 2; i++) {
+    if (ordPosA[i] < ordPosA[i + 1] + ordPosA[i + 2]) return 1;
+  }
+  return 0;
+}
