84d462ea923cc04b39933c708f9d07db3bf5446f
Halil Azyikmis <hazyi@halo-pc-hp.hacettepe.edu.tr>
2020-07-12 00:36:40 +0200

    Fish

--- src.js
+++ src.js
@@ -995,3 +995,83 @@ function brackets(S) {
   }
   return stack.length === 0 ? 1 : 0;
 }
+
+//# Fish
+//N voracious fish are moving along a river. Calculate how many fish are alive.
+/*
+You are given two non-empty arrays A and B consisting of N integers. Arrays A and B represent N voracious fish in a river, ordered downstream along the flow of the river.
+The fish are numbered from 0 to N − 1. If P and Q are two fish and P < Q, then fish P is initially upstream of fish Q. Initially, each fish has a unique position.
+Fish number P is represented by A[P] and B[P]. Array A contains the sizes of the fish. All its elements are unique. Array B contains the directions of the fish. It contains only 0s and/or 1s, where:
+0 represents a fish flowing upstream,
+1 represents a fish flowing downstream.
+If two fish move in opposite directions and there are no other (living) fish between them, they will eventually meet each other. Then only one fish can stay alive − the larger fish eats the smaller one. More precisely, we say that two fish P and Q meet each other when P < Q, B[P] = 1 and B[Q] = 0, and there are no living fish between them. After they meet:
+If A[P] > A[Q] then P eats Q, and P will still be flowing downstream,
+If A[Q] > A[P] then Q eats P, and Q will still be flowing upstream.
+We assume that all the fish are flowing at the same speed. That is, fish moving in the same direction never meet. The goal is to calculate the number of fish that will stay alive.
+For example, consider arrays A and B such that:
+  A[0] = 4    B[0] = 0
+  A[1] = 3    B[1] = 1
+  A[2] = 2    B[2] = 0
+  A[3] = 1    B[3] = 0
+  A[4] = 5    B[4] = 0
+Initially all the fish are alive and all except fish number 1 are moving upstream. Fish number 1 meets fish number 2 and eats it, then it meets fish number 3 and eats it too. Finally, it meets fish number 4 and is eaten by it. The remaining two fish, number 0 and 4, never meet and therefore stay alive.
+Write a function:
+function solution(A, B);
+that, given two non-empty arrays A and B consisting of N integers, returns the number of fish that will stay alive.
+For example, given the arrays shown above, the function should return 2, as explained above.
+Write an efficient algorithm for the following assumptions:
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [0..1,000,000,000];
+each element of array B is an integer that can have one of the following values: 0, 1;
+the elements of A are all distinct.
+*/
+//4 3 2 1 5
+//0 1 0 0 0
+//< > < < <
+
+//4 3 2 1 5
+//1 1 0 1 0
+//> > < > <
+
+//the only situation we should handle is coming 0 after 1
+//2 cases:
+//1-fish at the top of stack eats the coming fish - easy part
+//  -nothing to do, continue withe the next fish
+//2-coming fish eats the fish at the top of stack - hard part
+//  -pop the eaten fish from stack
+//  -compare the coming fish with the next top fish at the stack
+//    -continue until:
+//      -the coming fish eaten by a fish in the stack
+//      -the coming fish pushed to the stack (maybe nothing left in the stack)
+
+function fish(A, B) {
+  if (A.length <= 1) return A.length; // if there is only 1 fish or no fish
+  let stackA = []; //power
+  let stackB = []; //direction
+
+  stackA.push(A[0]);
+  stackB.push(B[0]);
+
+  for (i = 1; i < A.length; i++) {
+    //console.log(stackA, stackB);
+    if (B[i] === 0 && stackB[stackB.length - 1] === 1) {
+      //if (stackA[stackA.length-1] < A[i]) {
+      while (stackA[stackA.length - 1] < A[i]) {
+        stackA.pop();
+        stackB.pop();
+        if (stackB.length === 0 || stackB[stackB.length - 1] === 0) {
+          stackA.push(A[i]);
+          stackB.push(B[i]);
+          break; //while loop
+        }
+      }
+      //} //else do nothing, continue with the next coming fish
+    } else {
+      stackA.push(A[i]);
+      stackB.push(B[i]);
+    }
+  }
+  //console.log(stackA, stackB);
+  return stackA.length; // or return stackB.length;
+}
+
