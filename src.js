814b7f0346f72b6d99192456d5d1e3c5b448e929
Halil Azyikmis <hazyi@halo-pc-hp.hacettepe.edu.tr>
2020-07-11 17:14:34 +0200

    Brackets

--- src.js
+++ src.js
@@ -948,3 +948,50 @@ function triangle(A) {
   }
   return 0;
 }
+
+//# Brackets
+//Determine whether a given string of parentheses (multiple types) is properly nested.
+/*
+A string S consisting of N characters is considered to be properly nested if any of the following conditions is true:
+S is empty;
+S has the form "(U)" or "[U]" or "{U}" where U is a properly nested string;
+S has the form "VW" where V and W are properly nested strings.
+For example, the string "{[()()]}" is properly nested but "([)()]" is not.
+Write a function:
+function solution(S);
+that, given a string S consisting of N characters, returns 1 if S is properly nested and 0 otherwise.
+For example, given S = "{[()()]}", the function should return 1 and given S = "([)()]", the function should return 0, as explained above.
+Write an efficient algorithm for the following assumptions:
+N is an integer within the range [0..200,000];
+string S consists only of the following characters: "(", "{", "[", "]", "}" and/or ")".
+*/
+
+function brackets(S) {
+  //if opening then add stack, otherwise remove stack
+  let openingArr = ["{", "[", "("];
+
+  const openingBracket = (b) => {
+    switch (b) {
+      case "}":
+        return "{";
+      case "]":
+        return "[";
+      case ")":
+        return "(";
+    }
+  };
+
+  let stack = [];
+  for (let i = 0; i < S.length; i++) {
+    if (openingArr.includes(S[i])) {
+      stack.push(S[i]);
+    } else {
+      if (stack[stack.length - 1] === openingBracket(S[i])) {
+        stack.pop();
+      } else {
+        return 0;
+      }
+    }
+  }
+  return stack.length === 0 ? 1 : 0;
+}
