efad383aaf42c00fa94fc38ccad39ea95a4e518e
Halil Azyikmis <hazyi@halo-pc-hp.hacettepe.edu.tr>
2020-07-12 01:53:47 +0200

    Nesting

--- src.js
+++ src.js
@@ -1075,3 +1075,34 @@ function fish(A, B) {
   return stackA.length; // or return stackB.length;
 }
 
+//# Nesting
+//Determine whether a given string of parentheses (single type) is properly nested.
+/*
+A string S consisting of N characters is called properly nested if:
+S is empty;
+S has the form "(U)" where U is a properly nested string;
+S has the form "VW" where V and W are properly nested strings.
+For example, string "(()(())())" is properly nested but string "())" isn't.
+Write a function:
+function solution(S);
+that, given a string S consisting of N characters, returns 1 if string S is properly nested and 0 otherwise.
+For example, given S = "(()(())())", the function should return 1 and given S = "())", the function should return 0, as explained above.
+Write an efficient algorithm for the following assumptions:
+N is an integer within the range [0..1,000,000];
+string S consists only of the characters "(" and/or ")".
+*/
+
+function nesting(S) {
+  let stack = 0;
+  for (let i=0; i<S.length; i++) {
+    S[i] === '(' ? stack++ : stack--; 
+    
+    if (S[i] === '(') {
+      stack++
+    } else {
+      stack--;
+      if (stack < 0) return 0;
+    }
+  }
+  return stack === 0 ? 1 : 0;
+}
